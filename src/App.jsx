73c1e98468e153c17860c61bfb22f8624fe9647b
Rohit Vishwakarma <rohit8840@gmail.com>
2024-04-19 22:16:33 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'; // Import Navbar component
+import Hero from './Components/Hero/Hero';
+import Programs from './Components/Programs/Programs';
+import Title from './Components/Title/Title';
+
+const App = () => {
+  return (
+
+    <div>
+
+<Navbar/>
+<Hero/>
+<div className="container">
+  <Title subTitle='Our PROGRAM' Title='What We Offer'/>
+<Programs/>
+
+</div>
+      
+    </div>
+  )
+}
+
+export default App
